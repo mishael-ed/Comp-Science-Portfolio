c40a1db4f86eb97c8f0ea50b8f2623235ea666e2
Mishael Edegwa <mishaeledegwa@gmail.com>
2026-04-18 12:48:39 +0100

    x

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Contact from './pages/Contact.tsx'
 import ProjectAI from './pages/ProjectAI.tsx'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/contact" element={<Contact />} />
